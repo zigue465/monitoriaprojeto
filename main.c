44ee372d21bf2f2e1488b5d73d132467c489b47c
zigue465 <131168233+zigue465@users.noreply.github.com>
2023-04-18 11:16:19 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(vold) {
+  printf("\n BEM VINDO \nDIGITE UMA OPCAO \
+    \n1-LOGIN \
+    \n2-CADASTRO \
+    \n3-SAIR");
+
+  printf("\n OQUE DESEJA FAZER? \
+    \n1. perfil \
+    \n2. Tarefas \
+    \n3. Materias \
+    \n4. Plano de estudos \
+    \n5. Visao geral \
+    \n6. encerrar");
+
+  
+
+   
+   float atribuir;
+  return 0;
+    
+}
